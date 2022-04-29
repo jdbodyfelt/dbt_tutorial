a7fb11e83e60802286be7dce35e256640a4dbd92
J.D. Bodyfelt <josh.bodyfelt@quantiful.co.nz>
2022-04-29 12:40:05 +1200

    Add customers model, tests, docs

---
+++ models/stg_customers.sql
@@ -0,0 +1,5 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+from quantiful.jaffle_shop_raw.customers
---
+++ target/compiled/dbt_tutorial/models/schema.yml/not_null_stg_orders_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from "quantiful"."jaffle_shop"."stg_orders"
+where customer_id is null
+
+
